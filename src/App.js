2293290b8df8c2270af80ad31a0c3b234ce261a0
alinabezux <alinabezux@gmail.com>
2024-08-19 15:25:57 +0300

    final 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes, } from "react-router-dom";
+
 import MainLayout from "./components/MainLayout";
-// import Task1 from './components/task1/Task1';
-// import Task2 from './components/task2/Task2';
+
 
 const Task1 = React.lazy(() => import('./components/task1/Task1'));
 const Task2 = React.lazy(() => import('./components/task2/Task2'));
